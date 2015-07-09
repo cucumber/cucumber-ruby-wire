c913715de0d477d4470ac7f28e5dc119787c87dd
Matt Wynne <matt@mattwynne.net>
2015-07-10 00:34:17 +0200

    Extract class

--- lib/cucumber/wire.rb
+++ lib/cucumber/wire.rb
@@ -1,33 +1,5 @@
-require 'cucumber/wire/connections'
-
-module Cucumber
-  module Wire
-    class Installer
-      attr_reader :config
-      private :config
-
-      def initialize(config)
-        @config = config
-      end
-
-      def call
-        connections = Connections.new(wire_files.map { |wire_file| create_connection(wire_file) })
-        config.filters << Filters::ActivateSteps.new(connections)
-        config.register_snippet_generator Snippet::Generator.new(connections)
-      end
-
-      def create_connection(wire_file)
-        Connection.new(Configuration.from_file(wire_file))
-      end
-
-      def wire_files
-        # TODO: change Cucumber's config object to allow us to get this information
-        config.send(:require_dirs).map { |dir| Dir.glob("#{dir}/**/*.wire") }.flatten
-      end
-    end
-  end
-end
+require 'cucumber/wire/plugin'
 
 AfterConfiguration do |config|
-  Cucumber::Wire::Installer.new(config).call
+  Cucumber::Wire::Plugin.new(config).install
 end
---
+++ lib/cucumber/wire/plugin.rb
@@ -0,0 +1,29 @@
+require 'cucumber/wire/connections'
+
+module Cucumber
+  module Wire
+    class Plugin
+      attr_reader :config
+      private :config
+
+      def initialize(config)
+        @config = config
+      end
+
+      def install
+        connections = Connections.new(wire_files.map { |wire_file| create_connection(wire_file) })
+        config.filters << Filters::ActivateSteps.new(connections)
+        config.register_snippet_generator Snippet::Generator.new(connections)
+      end
+
+      def create_connection(wire_file)
+        Connection.new(Configuration.from_file(wire_file))
+      end
+
+      def wire_files
+        # TODO: change Cucumber's config object to allow us to get this information
+        config.send(:require_dirs).map { |dir| Dir.glob("#{dir}/**/*.wire") }.flatten
+      end
+    end
+  end
+end
