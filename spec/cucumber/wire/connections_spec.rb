19542486373fc9ca92f9c4970fba1fc6bfd9bf96
Matt Wynne <matt@mattwynne.net>
2015-09-26 16:40:45 +0100

    Whoops

--- spec/cucumber/wire/connections_spec.rb
+++ spec/cucumber/wire/connections_spec.rb
@@ -9,12 +9,12 @@ module Cucumber
           connection1 = double(step_matches: [:a, :b])
           connection2 = double(step_matches: [:c])
 
-          connections = Connections.new([connection1, connection2])
+          connections = Connections.new([connection1, connection2], double)
           expect(connections.step_matches('')).to eq [:a, :b, :c]
         end
 
         it "copes with no connections" do
-          connections = Connections.new([])
+          connections = Connections.new([], double)
           expect(connections.step_matches('')).to eq []
         end
       end
