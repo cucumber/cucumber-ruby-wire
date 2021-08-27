a428568cdd68bc9a3696bd81e56a9231bd9932cf
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-08-27 08:55:38 +0200

    Fix the check for InstallPlugin hook

--- lib/cucumber/wire.rb
+++ lib/cucumber/wire.rb
@@ -1,6 +1,6 @@
 require 'cucumber/wire/plugin'
 
-return unless defined?(InstallPlugin)
+return unless respond_to?(:InstallPlugin)
 
 InstallPlugin do |config, registry|
   Cucumber::Wire::Plugin.new(config, registry).install
