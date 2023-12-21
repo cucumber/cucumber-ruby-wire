60facdb2de8dc806a443a17774cf5e809b27004d
Luke Hill <lukehill_uk@hotmail.com>
2023-12-21 16:00:11 +0000

    AF: Layout/DotPosition

--- lib/cucumber/wire/request_handler.rb
+++ lib/cucumber/wire/request_handler.rb
@@ -22,11 +22,11 @@ module Cucumber
 
       # Props to Rails
       def underscore(camel_cased_word)
-        camel_cased_word.to_s.gsub(/::/, '/').
-        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-        gsub(/([a-z\d])([A-Z])/,'\1_\2').
-        tr('-', '_').
-        downcase
+        camel_cased_word.to_s.gsub(/::/, '/')
+        .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+        .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+        .tr('-', '_')
+        .downcase
       end
     end
   end
