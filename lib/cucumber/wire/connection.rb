646bec2b0fd40dd468d77af2e076fdbf03870168
Luke Hill <lukehill_uk@hotmail.com>
2024-09-26 15:38:25 +0100

    AF: UselessAssignment

--- lib/cucumber/wire/connection.rb
+++ lib/cucumber/wire/connection.rb
@@ -60,7 +60,7 @@ module Cucumber
         else
           @socket = TCPSocket.new(@config.host, @config.port)
         end
-      rescue Errno::ECONNREFUSED => e
+      rescue Errno::ECONNREFUSED
         raise(ConnectionError, "Unable to contact the wire server at #{@config}. Is it up?")
       end
     end
