2010d06bf7a5e2c6e0b0f510f38077ff4caa8b45
Luke Hill <lukehill_uk@hotmail.com>
2025-10-14 18:54:53 +0100

    Completely reformat connection_spec from #71

--- spec/cucumber/wire/connection_spec.rb
+++ spec/cucumber/wire/connection_spec.rb
@@ -3,63 +3,62 @@
 require 'cucumber/wire/connection'
 require 'cucumber/wire/configuration'
 
-module Cucumber
-  module Wire
-    describe Connection do
-      class TestConnection < Connection
-        attr_accessor :socket
+describe Cucumber::Wire::Connection do
+  subject(:connection) { test_connection.new(config) }
+
+  let(:test_connection) do
+    Class.new(described_class) do
+      attr_accessor :socket
+    end
+  end
+  let(:test_configuration) do
+    Class.new do
+      attr_reader :custom_timeout
+
+      def initialize
+        @custom_timeout = {}
       end
 
-      class TestConfiguration
-        attr_reader :custom_timeout
+      def timeout(message = nil)
+        return :default_timeout if message.nil?
 
-        def initialize
-          @custom_timeout = {}
-        end
-
-        def timeout(message = nil)
-          return :default_timeout if message.nil?
-
-          @custom_timeout[message] || Configuration::DEFAULT_TIMEOUTS.fetch(message)
-        end
-
-        def host
-          'localhost'
-        end
-
-        def port
-          '3902'
-        end
+        @custom_timeout[message] || Cucumber::Wire::Configuration::DEFAULT_TIMEOUTS.fetch(message)
       end
 
-      before(:each) do
-        @config = TestConfiguration.new
-        @connection = TestConnection.new(@config)
-        @connection.socket = @socket = double('socket').as_null_object
-        @response = '["response"]'
+      def host
+        'localhost'
       end
 
-      it 're-raises a timeout error' do
-        allow(Timeout).to receive(:timeout).and_raise(Timeout::Error.new(''))
-        expect { @connection.call_remote(nil, :foo, []) }.to raise_error(Timeout::Error)
-      end
-
-      it 'ignores timeout errors when configured to do so' do
-        @config.custom_timeout[:foo] = :never
-
-        allow(@socket).to receive(:gets) { @response }
-
-        handler = double(handle_response: :response)
-
-        expect(@connection.call_remote(handler, :foo, [])).to eq(:response)
-      end
-
-      it 'raises an exception on remote connection closed' do
-        @config.custom_timeout[:foo] = :never
-
-        allow(@socket).to receive(:gets)
-        expect { @connection.call_remote(nil, :foo, []) }.to raise_error(Wire::Exception, 'Remote Socket with localhost:3902 closed.')
+      def port
+        '3902'
       end
     end
+  end
+  let(:socket) { double('socket').as_null_object }
+  let(:config) { test_configuration.new }
+
+  before(:each) { connection.socket = socket }
+
+  it 're-raises a timeout error' do
+    allow(Timeout).to receive(:timeout).and_raise(Timeout::Error.new(''))
+
+    expect { connection.call_remote(nil, :foo, []) }.to raise_error(Timeout::Error)
+  end
+
+  it 'ignores timeout errors when configured to do so' do
+    config.custom_timeout[:foo] = :never
+
+    allow(socket).to receive(:gets).and_return('["response"]')
+
+    handler = double(handle_response: :response)
+
+    expect(connection.call_remote(handler, :foo, [])).to eq(:response)
+  end
+
+  it 'raises an exception on remote connection closed' do
+    config.custom_timeout[:foo] = :never
+    allow(socket).to receive(:gets)
+
+    expect { connection.call_remote(nil, :foo, []) }.to raise_error(Cucumber::Wire::Exception, 'Remote Socket with localhost:3902 closed.')
   end
 end
